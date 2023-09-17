9a599e3a49fe3aae3677675f43519a08f965cd3d
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2023-09-17 21:06:39 +0200

    fetch all artists

--- main.mjs
+++ main.mjs
@@ -1,11 +1,17 @@
-import { parseFile } from 'music-metadata';
-import { extname } from 'path';
-import { cacheFunction } from './lib/cache.mjs';
-import { walkDir } from './lib/fs-walk.mjs';
-import { fetchSimilarArtistsByArtistName } from "./lib/musicbrainz.mjs";
+import { parseFile } from "music-metadata";
+import { extname } from "path";
+import { cacheFunction } from "./lib/cache.mjs";
+import { walkDir } from "./lib/fs-walk.mjs";
+import { findArtist } from "./lib/musicbrainz.mjs";
 
-const MUSIC_EXTENSIONS = new Set(['.flac', '.mp3', '.ogg'])
+/**
+ * @typedef ArtistInformation
+ * @property {?string} [id]
+ * @property {string} name
+ * @property {string} [artistId]
+ */
 
+const MUSIC_EXTENSIONS = new Set([".flac", ".mp3", ".ogg"]);
 
 /**
  * @returns {Promise<Record<string, number>>}
@@ -14,58 +20,45 @@ async function getArtistCount() {
   /** @type {Record<string, number>} */
   const artistCount = {};
 
-  for await (const file of walkDir('/home/alexandre/Musique/')) {
+  for await (const file of walkDir("/home/alexandre/Musique/")) {
     if (!MUSIC_EXTENSIONS.has(extname(file))) continue;
 
     const metadata = await parseFile(file).catch(() => undefined);
-    if (!metadata) continue
+    if (!metadata) continue;
 
-    const artistName = metadata.common.artist ?? metadata.common.artists?.[0]
-    if (!artistName) continue
+    const artistName = metadata.common.artist ?? metadata.common.artists?.[0];
+    if (!artistName) continue;
 
-    const count = artistCount[artistName] ?? 0
-    artistCount[artistName] = count + 1
+    const count = artistCount[artistName] ?? 0;
+    artistCount[artistName] = count + 1;
   }
 
-  return artistCount
+  return artistCount;
 }
 
 /**
  * @returns {Promise<Record<string, number>>}
  */
-const getArtistCountWithCache = cacheFunction(getArtistCount)
+const getArtistCountWithCache = cacheFunction(getArtistCount);
 
+const findArtistWithCache = cacheFunction(findArtist);
 
 async function main() {
+  const artistCount = await getArtistCountWithCache();
 
-  const artistCount = await getArtistCountWithCache()
+  /** @type {ArtistInformation[]} */
+  const records = Object.entries(artistCount).map(([name, trackCount]) => ({
+    name,
+    trackCount,
+  }));
 
-  console.log(artistCount)
-  return
+  let i = 0;
 
-  let i = 0
-
-  for await (const file of walkDir('/home/alexandre/Musique/')) {
-
-    console.log(file, extname(file))
-
-    if (!['.flac', '.mp3'].includes(extname(file))) continue;
-
-    const metadata = await parseFile(file).catch(() => undefined)
-
-    const artistName = metadata.common.artist
-    if (!artistName) return []
-
-    const similarArtists = await fetchSimilarArtistsByArtistName(artistName)
-
-    for (const similarArtist of similarArtists) {
-      console.log(`${similarArtist.name} ${similarArtist.score}`)
-    }
-
-    i++
-
-    if (i === 5) return
+  for (const record of records) {
+    const artist = await findArtistWithCache(record.name);
+    if (!artist) continue;
+    record.artistId = artist.id;
   }
 }
 
-main().catch(console.error)
+main().catch(console.error);
