55f49adb544d83134a457a63f675b30156dcd4f3
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2023-09-17 15:23:37 +0200

    read artists count

--- main.mjs
+++ main.mjs
@@ -1,19 +1,62 @@
 import { parseFile } from 'music-metadata';
+import { extname } from 'path';
+import { walkDir } from './lib/fs-walk.mjs';
 import { fetchSimilarArtistsByArtistName } from "./lib/musicbrainz.mjs";
+
+const MUSIC_EXTENSIONS = new Set(['.flac', '.mp3', '.ogg'])
+
+
+async function getArtistCount() {
+  /** @type {Map<string, number>} */
+  const artistCount = new Map();
+
+  for await (const file of walkDir('/home/alexandre/Musique/')) {
+    if (!MUSIC_EXTENSIONS.has(extname(file))) continue;
+
+    const metadata = await parseFile(file).catch(() => undefined);
+    if (!metadata) continue
+
+    const artistName = metadata.common.artist ?? metadata.common.artists?.[0]
+    if (!artistName) continue
+
+    const count = artistCount.get(artistName) ?? 0
+    artistCount.set(artistName, count + 1)
+  }
+
+  return artistCount
+}
 
 
 async function main() {
-  const metadata = await parseFile('/home/alexandre/Musique/Daft\ Punk/1997\ -\ \ Homework/01\ Daftendirekt.flac')
 
-  const artistName = metadata.common.artist
-  if (!artistName) return []
+  const artistCount = await getArtistCount()
 
-  const similarArtists = await fetchSimilarArtistsByArtistName(artistName)
+  console.log(artistCount)
+  return
 
-  for (const similarArtist of similarArtists) {
-    console.log(`${similarArtist.name} ${similarArtist.score}`)
+  let i = 0
+
+  for await (const file of walkDir('/home/alexandre/Musique/')) {
+
+    console.log(file, extname(file))
+
+    if (!['.flac', '.mp3'].includes(extname(file))) continue;
+
+    const metadata = await parseFile(file).catch(() => undefined)
+
+    const artistName = metadata.common.artist
+    if (!artistName) return []
+
+    const similarArtists = await fetchSimilarArtistsByArtistName(artistName)
+
+    for (const similarArtist of similarArtists) {
+      console.log(`${similarArtist.name} ${similarArtist.score}`)
+    }
+
+    i++
+
+    if (i === 5) return
   }
-
 }
 
 main().catch(console.error)
